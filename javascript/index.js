e90329f067c2e7e0aa8c74aa5a2eac6390968ea5
Ayhamalahmad <amadae130@gmail.com>
2023-08-31 10:22:13 +0300

    Footer section  it's done

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
-console.log("from js");
+let bottons = document.querySelectorAll(".bottons li");
+let boxs = document.querySelectorAll(".main-box");
+// Add event listeners for image buttons
+bottons.forEach((botton) => {
+  botton.addEventListener("click",handleActive);
+  botton.addEventListener("click",handleBox);
+});
+
+// Update active state of buttons
+function handleActive()  {
+  bottons.forEach((botton) => {
+    botton.classList.remove("active");
+    this.classList.add("active");
+  });
+};
+// Change displayed box
+function handleBox(){
+    boxs.forEach((box)=>{
+        box.style.display="none";
+    })
+    document.querySelectorAll(this.dataset.filter).forEach((element)=>{
+        element.style.display = "flex";
+    })
+}
